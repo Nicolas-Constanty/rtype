1d037e9c2bf70629e50a8bdaae7782ae9d3bb038
Nicolas Constanty <nicolas.constanty@epitech.eu>
2016-11-28 17:03:42 +0100

    Add move sprite

--- Test/GUI/main.cpp
+++ Test/GUI/main.cpp
@@ -4,6 +4,8 @@
 #include "SaltyEngine/SFML/Renderer.hpp"
 #include "SaltyEngine/SFML/Button.hpp"
 #include "SaltyEngine/Debug.hpp"
+#include "SaltyEngine/Input.hpp"
+#include "SaltyEngine/PlayerController.hpp"
 
 int main(int ac, char **av)
 {
@@ -11,6 +13,7 @@ int main(int ac, char **av)
 
 	SaltyEngine::GameObject *player = new SaltyEngine::GameObject("Player");
 
+	// Create Button
 	SaltyEngine::SFML::Texture *texture = new SaltyEngine::SFML::Texture();
 	if (!texture->loadFromFile("../../Assets/Textures/Image.png"))
 	{
@@ -20,12 +23,14 @@ int main(int ac, char **av)
 	SaltyEngine::SFML::Rect *rect = new SaltyEngine::SFML::Rect(10, 10, 100, 100);
 	SaltyEngine::SFML::Sprite *spr = new SaltyEngine::SFML::Sprite(texture, rect);
 	player->AddComponent<SaltyEngine::GUI::SFML::Button>(spr);
+	player->AddComponent<SaltyEngine::PlayerController>();
 	// Set SFML Renderer
 	Singleton<SaltyEngine::SaltyEngine>::Instance().SetRenderer(renderer);
 
 	// Create Scene
 	SaltyEngine::Scene *scene(new SaltyEngine::Scene());
 
+	*scene << player;
 	// Push scene int SaltyEngine
 	Singleton<SaltyEngine::SaltyEngine>::Instance() << scene;
 
@@ -33,28 +38,3 @@ int main(int ac, char **av)
 	Singleton<SaltyEngine::SaltyEngine>::Instance().Run();
 	return (0);
 }
-
-//int main()
-//{
-//	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-//	sf::CircleShape shape(100.f);
-//	shape.setFillColor(sf::Color::Green);
-//
-//	//std::shared_ptr<SaltyEngine::Object> go = SaltyEngine::Object::Instantiate("DefaultMonster");
-//
-//	while (window.isOpen())
-//	{
-//		sf::Event event;
-//		while (window.pollEvent(event))
-//		{
-//			if (event.type == sf::Event::Closed)
-//				window.close();
-//		}
-//
-//		window.clear();
-//		window.draw(shape);
-//		window.display();
-//	}
-//
-//	return 0;
-//}
---
+++ include/SaltyEngine/Engine.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef ENGINE_HPP_
+#define ENGINE_HPP_
+
+#include "Input.hpp"
+#include "Graphics.hpp"
+
+#endif // !ENGINE_HPP_
