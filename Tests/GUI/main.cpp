e7cbf0eff7923263f4916324424c1f9590f689fb
Fernand <fernand@yhapps.com>
2016-12-07 23:20:06 +0100

    Fixed compile linux

--- Tests/GUI/main.cpp
+++ Tests/GUI/main.cpp
@@ -15,8 +15,9 @@ namespace SaltyEngine {
 
 int main()
 {
-	SaltyEngine::AssetManager.LoadAssets();
-	SaltyEngine::SFML::Renderer *renderer = new SaltyEngine::SFML::Renderer(sf::VideoMode(1280, 720), "R-Type Launcher");
+    SaltyEngine::AssetManager.LoadAssets();
+
+    SaltyEngine::SFML::Renderer *renderer = new SaltyEngine::SFML::Renderer(sf::VideoMode(1280, 720), "R-Type Launcher");
 	SaltyEngine::SFML::EventManager *event_manager = new SaltyEngine::SFML::EventManager(renderer->GetRenderWindow());
 	// Set Renderer and EventManager
 	Singleton<SaltyEngine::SaltyEngine>::Instance().SetRenderer(renderer);
--- src/Monster/Monster.cpp
+++ src/Monster/Monster.cpp
@@ -17,7 +17,6 @@
 Monster::Monster() : GameObject("Monster")
 {
     SaltyEngine::SFML::Texture *texture = SaltyEngine::SFML::AssetManager::Instance().GetTexture("monster");
-    std::cout << "Texture > " << texture << std::endl;
     if (texture != nullptr)
     {
         AddComponent<SaltyEngine::SFML::SpriteRenderer>(new SaltyEngine::SFML::Sprite(texture, new SaltyEngine::SFML::Rect(0, 0, 34, 34)), SaltyEngine::Layout::normal);
@@ -28,19 +27,9 @@ Monster::Monster() : GameObject("Monster")
         clip->AddSprite(new SaltyEngine::SFML::Sprite(texture, new SaltyEngine::SFML::Rect(68, 0, 34, 34)));
         clip->SetFrameRate(5);
         GetComponent<SaltyEngine::Animation<sf::Vector2i> >()->AddClip(clip, "Walk");
-//
-//
-//        SaltyEngine::SFML::Sprite *spr = new SaltyEngine::SFML::Sprite(texture, new SaltyEngine::SFML::Rect(0, 0, 34, 34)), SaltyEngine::Layout::normal);
-//        this->AddComponent<SaltyEngine::SFML::SpriteRenderer>(spr, SaltyEngine::Layout::normal);
-//        SaltyEngine::AnimationClip<sf::Vector2i> *clip = new SaltyEngine::AnimationClip<sf::Vector2i>("WalkAnim", 10);
-//        for (int i = 0; i < 5; ++i)
-//        {
-//            *clip << new SaltyEngine::SFML::Sprite(texture, new SaltyEngine::SFML::Rect(10 * i, 10 * i, 100, 100));
-//        }
-//        this->AddComponent < SaltyEngine::Animation<sf::Vector2i> >(true, SaltyEngine::AnimationConstants::WrapMode::LOOP);
-//        this->GetComponent< SaltyEngine::Animation<sf::Vector2i> >()->AddClip(clip, "Walk");
-
-    } else {
+    }
+    else
+    {
         SaltyEngine::Debug::PrintWarning("Monster: could not load texture");
     }
 	AddComponent<MonsterController>();
